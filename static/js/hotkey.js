9b923d15cd7cdc18f72cf521903167c82491bac6
Aman Agrawal <amanagr@zulip.com>
2020-09-20 13:44:24 +0530

    recent_topics: Make it the default view.
    Go to Recent Topics on "#", no hash and "#recent_topics".
    Go to Recent Topics as the last destination for escape key.
    Bing `a` key to All messages and change its hash to
    `#all_messages`.

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -94,6 +94,7 @@ const keypress_mappings = {
     82: {name: "respond_to_author", message_view_only: true}, // 'R'
     83: {name: "narrow_by_topic", message_view_only: true}, //'S'
     86: {name: "view_selected_stream", message_view_only: false}, //'V'
+    97: {name: "all_messages", message_view_only: true}, // 'a'
     99: {name: "compose", message_view_only: true}, // 'c'
     100: {name: "open_drafts", message_view_only: true}, // 'd'
     101: {name: "edit_message", message_view_only: true}, // 'e'
@@ -268,7 +269,7 @@ exports.process_escape_key = function (e) {
         return true;
     }
 
-    hashchange.go_to_location("");
+    hashchange.go_to_location("#");
     return true;
 };
 
@@ -468,7 +469,7 @@ exports.process_hotkey = function (e, hotkey) {
         case "tab":
         case "shift_tab":
             if (
-                window.location.hash === "#recent_topics" &&
+                ["#recent_topics", "#", ""].includes(window.location.hash) &&
                 !popovers.any_active() &&
                 !overlays.is_active()
             ) {
@@ -726,6 +727,9 @@ exports.process_hotkey = function (e, hotkey) {
             return true;
         case "open_recent_topics":
             hashchange.go_to_location("recent_topics");
+            return true;
+        case "all_messages":
+            hashchange.go_to_location("#all_messages");
             return true;
     }
 
